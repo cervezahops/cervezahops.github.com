6271c47299f17543f478ca6973ae27bf676eb579
Ignacio Lezama <ignaciolezama@gmail.com>
2015-05-19 21:35:18 -0300

    Add placeholder
    Add html file with logo, to use as a placeholder for the site.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,1 @@
-window.addEventListener('scroll', function(){
 
-  var wScroll = this.pageYOffset;
-
-  if (wScroll < 800) {
-  	document.querySelector('.logo').style.
-     transform = 'translate(0, -'+ wScroll /20 +'%)';
-  };
-
-
-	if (wScroll > 800) {
-    document.querySelector('nav').style.
-    transform = 'translate(0, 0)';
-  } else {
-    document.querySelector('nav').style.
-    transform = 'translate(0, -56px)';
-  };
-
-});
